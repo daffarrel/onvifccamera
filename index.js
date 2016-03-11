43588adfeb6a6f6f0e8ba2ae928ea1f54da68cbc
susheel singh <susheel61@gmail.com>
2016-03-11 20:04:24 +0530

    resetting

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
-var Cam = require('onvif').Cam,
-express = require('express');
+var express = require('express');
 var app = express();
 
-// var cam = new Cam({hostname: '192.168.1.38',username: 'admin',password: 'admin',port:'2000'});
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function (req, res) {
-	// cam.getStreamUri({protocol:'RTSP'}, function(err, stream) {
-		// res.send('<html><body><embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed></boby></html>');
-	// });
-	res.send("helloworld");
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
 });
 
-// app.get('/move', function (req, res) {
-	// var x = req.query.x;
-	// var y = req.query.y;
-	// cam.continuousMove({x:x,y:y,zoom:1});
-	// res.send("moving the camera");
-// });
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-}); 
+
