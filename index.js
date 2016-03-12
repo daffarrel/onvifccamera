3c6a35e718e4493391ff5d5333e5461b74292e99
susheel singh <susheel61@gmail.com>
2016-03-12 16:36:40 +0530

    testing new chaNGES

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
-// var Cam = require('onvif').Cam;
-// var cam = new Cam({hostname: '176.86.90.106',username: 'admin',password: 'admin',port:'2000'});
+var Cam = require('onvif').Cam;
+var cam = new Cam({hostname: '192.168.1.38',username: 'admin',password: 'admin',port:'2000'});
 
-app.get('/', function (req, res) {
-	// cam.getStreamUri({protocol:'RTSP'}, function(err, stream) {
-		// res.send('<html><body><embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed></boby></html>');
-	// });
-	console.log("hello");
-	res.send("helloooooooo");
+app.get('/livestreaming', function (req, res) {
+	cam.getStreamUri({protocol:'RTSP'}, function(err, stream) {
+		res.send('<embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed>');
+	});
 });
 
-app.get('/move', function (req, res) {
+app.get('/movecamera', function (req, res) {
 	var x = req.query.x;
 	var y = req.query.y;
 	cam.continuousMove({x:x,y:y,zoom:1});
