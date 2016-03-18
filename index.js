bbb614e1730d822927f85121747c88c38c2d01b0
susheel <susheel61@gmail.com>
2016-03-18 05:42:22 +0100

    added search,connect functionality

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
 var express = require('express');
 var cors = require('cors')
 var app = express();
+var onvif = require('onvif');
+var bodyParser = require('body-parser')
 app.use(cors());
-
+var jsonParser = bodyParser.json()
+app.use(bodyParser.json({ type: 'application/*+json' }))
 app.set('port', (process.env.PORT || 5000));
 
 var Cam = require('onvif').Cam;
-var cam = new Cam({hostname: '192.168.1.38',username: 'admin',password: 'admin',port:'2000'});
+var cam = null;
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+}); 
+
+app.get('/search', function (req, res) {
+	onvif.Discovery.probe(function(err, cams) {
+	// function would be called only after timeout (5 sec by default) 
+		if (err) { throw err; }	
+		var jsonObject ='[';
+		var count = 0;
+		var len = cams.length;
+		cams.forEach(function(cam) {
+			count++;
+			jsonObject +='{';
+			jsonObject +='"hostname":"'+cam.hostname+'",';
+			jsonObject +='"username":"'+cam.username+'",';
+			jsonObject +='"password":"'+cam.password+'",';
+			jsonObject +='"port":"'+cam.port+'"';
+			if(count==len)
+				jsonObject +="}";
+			else
+				jsonObject +="},";		
+		});	
+		jsonObject +=']';
+		console.log(jsonObject);	
+		res.json(JSON.parse(jsonObject));		
+	});
+});
+
+app.post('/connect',jsonParser, function (req, res) {
+	cam = new Cam(req.body);	
+	res.send("connected successfully");
+});
 
 app.get('/livestreaming', function (req, res) {
 	cam.getStreamUri({protocol:'RTSP'}, function(err, stream) {
+	console.log(err);
 		res.send('<embed type="application/x-vlc-plugin" target="' + stream.uri + '"></embed>');
 	});
 });
@@ -21,9 +59,11 @@ app.get('/movecamera', function (req, res) {
 	res.send("moving the camera");
 });
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
-}); 
+app.delete('/disconnect', function (req, res) {
+	cam = null;
+	res.send("disconnected");
+});
+
 
 
 
